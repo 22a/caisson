c2f1d7932c036a82f4a62364aa0f6871d442c64c
Peter Meehan <meehanp2@tcd.ie>
2017-03-10 00:44:45 +0000

    implemented time limit enforcement

--- bin/execute.sh
+++ bin/execute.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # rename args for readability, there's probably a better way to do this...
 # TODO: find that better way
 lang=$1
@@ -10,13 +12,23 @@ docker_image=$6
 # set up execution env TODO: apply sec patches
 # spawn container from image
 con_hash=$(sudo docker run -dt --network=none --user=nobody --cap-drop=all $docker_image /bin/sh)
+
 # inject procedure script
 sudo docker cp $proc_path $con_hash:/exec.sh
 # inject payload
 sudo docker cp $payload_path $con_hash:/payload
 
-# compile/execute the payload TODO: incorporate limits here
-sudo docker exec $con_hash /bin/sh exec.sh
+# capture output from payload execution, enforce timeout
+# send SIGKILL to make docker play ball and exit
+output=$(sudo timeout -s 9 $time_limit sudo docker exec $con_hash /bin/sh exec.sh)
+# capture exit code from above timeout
+timeout_exit_code=$?
 
 # kill the container
-sudo docker rm -f $con_hash > /dev/null
+sudo docker rm -f $con_hash &> /dev/null
+
+# TODO: consider not storing output and echoing later
+echo $output
+
+# pass exit code back up to calling process
+exit $timeout_exit_code
